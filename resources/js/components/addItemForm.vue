3dbd6ffbd454c043f5b4ac450219bf7f50c86a6e
RedaMeziouni <meziounidb@gmail.com>
2021-07-12 14:40:56 +0100

    setting up axios

--- resources/js/components/addItemForm.vue
+++ resources/js/components/addItemForm.vue
@@ -1,13 +1,36 @@
 <template>
   <div class="addItem">
-      <input type="text" class="input" placeholder="Add Todo">
-      <button class="addButton"></button>
+      <input type="text" class="input" placeholder="Add Todo" v-model="item.name">
+      <button class="addButton" @click="addItem()">
+          <font-awesome-icon
+          icon="plus"
+          class="plus" />
+      </button>
   </div>
 </template>
 
 <script>
 export default {
-
+    data: function() {
+        return {
+            item: {
+                name: ""
+            }
+        }
+    },
+    methods: {
+        addItem() {
+            if(this.item.name !== '') {
+                axios.post('api/todo/store', {
+                    todo: this.item.name
+                }).then(response => {
+                    if(response.status >= 200 && response.status < 300) {
+                        this.item.name = null
+                    }
+                })
+            }
+        }
+    }
 }
 </script>
 
@@ -39,5 +62,13 @@ export default {
         border-top-right-radius: 4px;
         border-width: 1px;
         background-color: white;
+    }
+    button {
+        cursor: pointer;
+    }
+
+    .plus {
+        font-size: 20px;
+        color: blue;
     }
 </style>

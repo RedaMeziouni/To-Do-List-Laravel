a7a56413c434eaf264164fb2b9b54ecd4b062668
RedaMeziouni <meziounidb@gmail.com>
2021-07-12 13:48:43 +0100

    Add icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,10 @@ import App from './components/App'
 
 // Import FontAwesome
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faUserSecret)
+library.add(faPlus)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 /**

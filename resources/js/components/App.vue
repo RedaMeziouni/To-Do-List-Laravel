7664a9d7d5e56ab01d172d5d061712233cc9ebaa
RedaMeziouni <meziounidb@gmail.com>
2021-07-12 13:05:52 +0100

    Setting up the App.vue

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,9 +1,28 @@
 <template>
-    <div></div>
+    <div>
+        <h1 style="margin-top:200px; text-align: center">To Do List</h1>
+        <div class="todoListContainer">
+            <add-item-form />
+            <list-view />
+        </div>
+    </div>
 </template>
 
 <script>
+import addItemForm from './addItemForm'
+import addItemsForm from './addItemForm.vue'
+import listView from './listView'
     export default {
-
+  components: { addItemsForm },
+        addItemForm,
+        listView
     }
 </script>
+
+<style scoped>
+    .todoListContainer {
+        width: 400px;
+        margin: auto;
+        margin-top: 50px;
+    }
+</style>

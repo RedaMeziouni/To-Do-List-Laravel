902ef1e79d0343ebb808c6692570d02eca46cfa5
RedaMeziouni <meziounidb@gmail.com>
2021-07-12 16:06:11 +0100

    Routers 101

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/todos', [TodoController::class, 'index']);
+Route::get('todos', [TodoController::class, 'index']);
 
 
 Route::post('todo/store', [TodoController::class, 'store']);

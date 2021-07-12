49fd7da82a833652145fd224e3750ff2287306ee
RedaMeziouni <meziounidb@gmail.com>
2021-07-12 12:05:05 +0100

    Store controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('todos', [TodoController::class, 'index']);
+
+
+Route::post('todo/store', [TodoController::class, 'store']);
